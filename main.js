9db6b10e351f4407fbe3f5abea4f849f9466d41e
Yashwant Belgahe <yashbelgahe99@gmail.com>
2024-09-12 21:00:35 +0530

    created gemini

---
+++ main.js
@@ -0,0 +1,45 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World! Gemini');
+})
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+//const prompt = "tell me a joke";
+
+const generate = async (prompt) => {
+    try {
+        const result = await model.generateContent(prompt);
+        console.log(result.response.text());
+        return result.response.text();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//generate();
+
+app.get('/api/content', async(req, res) => {
+    try {
+        const data = req.body.question;
+        const result = await generate(data);
+        res.send({
+            "result": result
+        })
+    } catch (error) {
+        res.send("error: " + error)
+        
+    }
+})
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+})
